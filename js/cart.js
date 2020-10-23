2f22be5ef88c52ca19c95f2329430b718a169ba2
Victor Regueira <68743679+victor1mrs@users.noreply.github.com>
2020-10-23 13:12:04 -0300

    carrito con envio

--- js/cart.js
+++ js/cart.js
@@ -19,6 +19,21 @@ function calcSubtotal(precio, i, moneda){
     calcTotal();
 }
 
+function eliminar(i){
+    if(productsArray.length>1){
+        productsArray.splice(i,1); //el segundo parametro es la cantidad de objetos que queremos borrar
+        showProducts(productsArray);
+    }else{
+        document.getElementById("carrito").innerHTML = `
+        <div class="text-center">
+        <br>
+        <h2>No hay productos agregados al carrito</h2>
+        <p>Solicite uno de nuestro <a href="products.html">listado</a></p>
+        </div>
+        `;
+    }
+}
+
 function showProducts(array){
     let contenido = "";
     for(let i = 0; i<array.length;i++){
@@ -41,11 +56,28 @@ function showProducts(array){
             type="number" id="cantidad${i}" value="${product.count}" min="1"></td>
             
             <td><span class="subtotal" id="productSubtotal${i}" style="font-weight:bold; color:blue;">${sub}</span></td>
+            <td><button class="btn btn-danger" onclick="eliminar(${i})">&times;</button></td>
         </tr>
         `
         document.getElementById("listado").innerHTML = contenido;
     }
     calcTotal();
+}
+
+function calcEnvio(){
+    let total = parseInt(document.getElementById("total").innerHTML);
+    let envio;
+    let elements = document.getElementsByName("envio");
+    for(var i=0;i<elements.length;i++){
+        if(elements[i].checked){
+            envio = parseInt(elements[i].value);
+        }
+    }
+    let precioEnvio = Math.ceil(envio*total/100);
+    let totalConEnvio = total + precioEnvio;
+    
+    let contenido = `Precio del envío: $${precioEnvio}<br><strong>Total con envío: $${totalConEnvio} </strong><br>` ;
+    document.getElementById("totalEnvio").innerHTML = contenido;
 }
 
 
@@ -54,7 +86,15 @@ document.addEventListener("DOMContentLoaded", function(e){
         if(resultObj.status === "ok"){
             productsArray = resultObj.data.articles;
             showProducts(productsArray);
+            calcEnvio()
         }
     });
+
+    let elements = document.getElementsByName("envio");
+    for(var i=0;i<elements.length;i++){
+        elements[i].addEventListener("change", function(){
+            calcEnvio()
+        });
+    }
 
 });
